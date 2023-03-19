6222197f530f048704673766444890a9677bf254
Glaz-z <mamdoh.v66@gmail.com>
2023-03-19 15:25:31 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
+ * main  - Entry point
+ *
+ * Description: print a quote using write function
+ * main return value  = 1 (error)
+*/
+
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo.59);
 	return (1);
 }
